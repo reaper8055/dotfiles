46122ae1c737e7f3e31b4d504007c9844dcd331d
reaper8055 <11490705+reaper8055@users.noreply.github.com>
2024-11-10 11:35:58 +0530

    updated dots

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,53 +1,76 @@
 -- :help options
+
+-- Enable editorconfig support
+vim.g.editorconfig = true
+
 -- do not create a backup file
 vim.opt.backup = false
+
 -- allows neovim to access the system clipboard
 vim.opt.clipboard = "unnamedplus"
+
 -- more space in the neovim command line for displaying messages
 vim.opt.cmdheight = 1
+
 -- mostly just for cmp
 vim.opt.completeopt = { "menuone", "noselect" }
+
 -- so that `` is visible in markdown files
 vim.opt.conceallevel = 0
 vim.opt.encoding = "utf-8"
+
 -- highlight all matches on previous search pattern
 vim.opt.hlsearch = true
+
 -- ignore case in search patterns
 vim.opt.ignorecase = true
+
 -- allow the mouse to be used in neovim
 vim.opt.mouse = "a"
--- pop up menu height vim.opt.showmode = false
--- I don't need to see things like -- INSERT -- anymore
+
+-- pop up menu height
 vim.opt.pumheight = 10
+
 -- always show tabs
 vim.opt.showtabline = 2
+
 -- smart case
 vim.opt.smartcase = true
+
 -- make indenting smarter again
 vim.opt.smartindent = true
+
 -- force all horizontal splits to go below current window
 vim.opt.splitbelow = true
+
 -- force all vertical splits to go to the right of current window
 vim.opt.splitright = true
+
 -- do not create a swapfile
 vim.opt.swapfile = false
+
 -- set term gui colors (most terminals support this)
 vim.opt.termguicolors = true
+
 -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.opt.timeoutlen = 1000
+
 -- enable persistent undo
 vim.opt.undofile = true
+
 -- faster completion (4000ms default)
 vim.opt.updatetime = 300
+
 -- if a file is being edited by another program (or was written to file while editing
 -- with another program), it is not allowed to be edited
 vim.opt.writebackup = false
--- convert tabs to spaces
-vim.opt.expandtab = true
--- the number of spaces inserted for each indentation
-vim.opt.shiftwidth = 2
--- insert 2 spaces for a tab
-vim.opt.tabstop = 2
+
+-- Indentation settings (these will be overridden by EditorConfig)
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 2 -- insert 2 spaces for a tab
+vim.opt.softtabstop = 2 -- make sure softtabstop matches tabstop
+
 -- highlight the current line
 vim.opt.cursorline = true
 -- set numbered lines
@@ -75,10 +98,31 @@ vim.opt.listchars:append({
   trail = "·",
   eol = "¬",
 })
+
+-- End of line settings (these will be overridden by EditorConfig)
+vim.opt.fileformats = "unix,dos,mac" -- Prefer Unix line endings
+vim.opt.endofline = true -- Ensure file ends with newline
+vim.opt.fixendofline = true -- Fix files lacking final newline
+
 -- notify plugin
 vim.notify = require("notify")
 
+-- Allow specified keys that move the cursor left/right to move to the previous/next line when the cursor is on the first/last character
+-- <,> = left and right arrow keys
+-- [,] = cursor keys in insert mode
+-- h,l = h and l keys in normal mode
 vim.cmd("set whichwrap+=<,>,[,],h,l")
+
+-- Add hyphen (-) to the list of characters that are considered part of a word
+-- This means operations like 'w', 'b', or '*' will treat hyphenated-words as a single word
 vim.cmd([[set iskeyword+=-]])
-vim.cmd("set formatoptions+=t")
--- vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
+
+-- Modify format options:
+-- t = auto-wrap text using textwidth
+-- Note: Other format options you might want to consider:
+-- c = auto-wrap comments using textwidth
+-- q = allow formatting of comments with "gq"
+-- j = remove comment leader when joining lines
+-- r = automatically insert comment leader after hitting <Enter>
+-- n = recognize numbered lists
+-- vim.cmd("set formatoptions+=t")
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,46 +7,60 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
+  config:set_strict_mode(true)
 end
 
 -- This is where you actually apply your config choices
 -- config.color_scheme = "Ayu Mirage"
 
+config.command_palette_bg_color = "#111116"
+config.command_palette_fg_color = "#dcd7ba"
+config.command_palette_rows = 10
+config.command_palette_font_size = 12
+config.window_frame = {
+  font = wezterm.font({
+    family = "FiraCode Nerd Font",
+    weight = 450,
+  }),
+}
+
+-- config.default_prog = { "tmux" }
+config.scrollback_lines = 1000000
 config.force_reverse_video_cursor = true
 config.color_scheme = "kanagawa (Gogh)"
 config.enable_tab_bar = true
 config.font = wezterm.font({
-	family = "FiraCode Nerd Font",
-	weight = 450,
+  family = "FiraCode Nerd Font",
+  weight = 450,
 })
 config.font_rules = {
-	{
-		italic = true,
-		font = wezterm.font({
-			family = "JetBrainsMono Nerd Font",
-			style = "Italic",
-		}),
-	},
-	{
-		italic = false,
-		intensity = "Bold",
-		font = wezterm.font({
-			family = "FiraCode Nerd Font",
-		}),
-	},
+  {
+    italic = true,
+    font = wezterm.font({
+      family = "JetBrainsMono Nerd Font",
+      style = "Italic",
+    }),
+  },
+  {
+    italic = false,
+    intensity = "Bold",
+    font = wezterm.font({
+      family = "FiraCode Nerd Font",
+    }),
+  },
 }
 config.enable_wayland = false
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.tab_max_width = 60
-config.font_size = 12
-config.freetype_load_flags = "NO_HINTING"
+-- config.font_size = 12
+-- config.freetype_load_flags = "NO_HINTING"
 
 config.window_padding = {
-	left = 10,
-	right = 10,
-	top = 1,
-	bottom = 1,
+  left = 8,
+  right = 5,
+  top = 1,
+  bottom = 1,
 }
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = true
@@ -57,15 +71,15 @@ config.check_for_updates = true
 config.check_for_updates_interval_seconds = 86400
 config.show_update_window = true
 config.skip_close_confirmation_for_processes_named = {
-	"bash",
-	"sh",
-	"zsh",
-	"fish",
-	"tmux",
-	"nu",
-	"cmd.exe",
-	"pwsh.exe",
-	"powershell.exe",
+  "bash",
+  "sh",
+  "zsh",
+  "fish",
+  "tmux",
+  "nu",
+  "cmd.exe",
+  "pwsh.exe",
+  "powershell.exe",
 }
 
 -- and finally, return the configuration to wezterm
